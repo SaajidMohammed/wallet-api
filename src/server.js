c65271e68a155524b259f7f02b7e7b3e5d3f186e
SaajidMohammed <saajidmohammed6@gmail.com>
2025-09-27 14:51:21 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,8 +8,15 @@ const job = require("./config/cron.js");
 
 dotenv.config();
 
+
 const app = express();
+
+if(process.env.NODE_ENV === "production") job.start();
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health",(req,res) => {
+    res.status(200).json({status:"ok"})
+})
 
 // --- Middleware Setup ---
 // Apply middleware before any routes
